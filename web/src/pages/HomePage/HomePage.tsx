f090b788939728c73a9fb5f0491afbd7dfb16699
Will Schaller <willschaller@gmail.com>
2022-10-07 14:46:42 +0200

    Initial commit

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default HomePage
