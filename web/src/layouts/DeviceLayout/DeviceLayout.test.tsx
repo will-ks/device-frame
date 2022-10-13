d9c8f20e5554a111ec772c8cdcf21ff971de8ace
Will Schaller <willschaller@gmail.com>
2022-10-13 13:59:23 +0200

    Fix tests and add tests for each combination of device and display mode

--- web/src/layouts/DeviceLayout/DeviceLayout.test.tsx
+++ web/src/layouts/DeviceLayout/DeviceLayout.test.tsx
@@ -1,5 +1,7 @@
 import { render } from '@redwoodjs/testing/web'
 
+import { DEFAULTS } from 'src/pages/FramePage/FramePage'
+
 import DeviceLayout from './DeviceLayout'
 
 //   Improve this test with help from the Redwood Testing Doc:
@@ -8,7 +10,23 @@ import DeviceLayout from './DeviceLayout'
 describe('DeviceLayout', () => {
   it('renders successfully', () => {
     expect(() => {
-      render(<DeviceLayout />)
+      render(
+        <DeviceLayout
+          deviceId={DEFAULTS.device}
+          url={DEFAULTS.url}
+          themeColor={DEFAULTS.themeColor}
+          displayMode={DEFAULTS.displayMode}
+          deviceScreenHeight={700}
+          attribution={{
+            licenceName: 'test',
+            licenceUrl: 'https://example.com',
+            authorUrl: 'https://example.com',
+            imageName: 'test',
+            authorName: 'test',
+            imageUrl: 'https://example.com',
+          }}
+        />
+      )
     }).not.toThrow()
   })
 })
--- web/src/pages/FramePage/FramePage.test.tsx
+++ web/src/pages/FramePage/FramePage.test.tsx
@@ -1,14 +1,30 @@
 import { render } from '@redwoodjs/testing/web'
 
-import FramePage from './FramePage'
+import FramePage, { DEFAULTS, devices, DisplayMode } from './FramePage'
 
 //   Improve this test with help from the Redwood Testing Doc:
 //   https://redwoodjs.com/docs/testing#testing-pages-layouts
 
 describe('FramePage', () => {
-  it('renders successfully', () => {
+  it('renders successfully with no path', () => {
     expect(() => {
-      render(<FramePage url={'https://example.com'} />)
+      render(<FramePage path={''} />)
+    }).not.toThrow()
+  })
+})
+
+describe.each([
+  ...Object.keys(devices).flatMap((d) =>
+    Object.values(DisplayMode).map((v) => [d, v])
+  ),
+])('FramePage', (deviceId, displayMode) => {
+  it(`renders successfully with path, device ${deviceId}, display mode ${displayMode}`, () => {
+    expect(() => {
+      render(
+        <FramePage
+          path={`${deviceId}/${displayMode}/${DEFAULTS.themeColor}/${DEFAULTS.url}`}
+        />
+      )
     }).not.toThrow()
   })
 })
